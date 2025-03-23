85b44f648403949bc749f92fe341e133e6aa7085
anhoangphuc <hoangphucnb97@gmail.com>
2025-03-23 11:09:48 +0700

    feat(header): add content and highlight for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,31 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+const NavLink = ({ href, children}: { href: string, children: React.ReactNode }) => {
+  return (
+    <Link href={href} className="relative group">
+      <span className="relative z-10">{children}</span>
+      <span className="absolute bottom-[-4px] left-0 w-full h-0 bg-yellow-300 transition-all duration-300 group-hover:h-0.5 -z-10"></span>
+    </Link>
+  )
+}
+
 const Header = () => {
   return (
-    <header className="w-full py-4 shadow-md">
-      <div className="container mx-auto flex justify-between items-center">
+    <header className="fixed top-0 left-0 w-full py-4 shadow-md z-10">
+      <div className="container mx-auto flex items-center relative">
         <Link href="/" className="flex items-center">
           <Image src="/vercel.svg" alt="Logo" width={40} height={40} />
-          <span className="ml-2 text-xl font-bold">PhucTBH</span>
+          <span className="ml-2 text-2xl font-bold">PhucTBH</span>
         </Link>
         
-        <nav>
+        <nav className="absolute left-1/2 transform -translate-x-1/2 text-2xl">
           <ul className="flex space-x-6">
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/blockchain">Blockchain</Link></li>
-            <li><Link href="/software">Software</Link></li>
-            <li><Link href="/story">Story</Link></li>
-            <li><Link href="/about">About</Link></li>
+            <li><NavLink href='/'>Home</NavLink></li>
+            <li><NavLink href="/blockchain">Blockchain</NavLink></li>
+            <li><NavLink href="/software">Software</NavLink></li>
+            <li><NavLink href="/program">Program</NavLink></li>
+            <li><NavLink href="/story">Story</NavLink></li>
           </ul>
         </nav>
       </div>
