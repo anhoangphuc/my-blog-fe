0e03508088b3c7b4045848d51f23dcd00db02e3b
anhoangphuc <hoangphucnb97@gmail.com>
2025-03-27 20:28:23 +0700

    feat(build): setup eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -9,6 +11,34 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')]
+const prettierPluginConfig = {
+  plugins: {
+    prettier: prettierPlugin,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    ...prettierPlugin.configs.recommended.rules,
+  },
+}
+const ignores = [
+  '**/node_modules/**',
+  '**/dist/**',
+  '**/build/**',
+  '**/.next/**',
+  '**/out/**',
+  '**/coverage/**',
+  '**/.git/**',
+  '**/*.min.js',
+  '**/public/**',
+]
+
+const eslintConfig = [
+  {
+    ignores,
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  prettierPluginConfig,
+  prettierConfig,
+]
 
 export default eslintConfig
